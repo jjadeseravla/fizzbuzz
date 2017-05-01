f9d26a72c026197259e2ecb5c7e484fa3c4eeb37
jade <jadealvares@hotmail.co.uk>
2017-05-01 14:28:45 +0100

    test divisible by 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,9 @@ describe "fizzbuzz" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
 end
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when passed 6" do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+end
